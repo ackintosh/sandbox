554bd8b809a17007d0b3f39c54107fcb96fef955
akihito.nakano <sora.akatsuki@gmail.com>
2020-02-02 12:15:54 +0900

    Query TXT records

--- rust/dns/src/main.rs
+++ rust/dns/src/main.rs
@@ -1,14 +1,21 @@
+use std::str::FromStr;
 use std::net::ToSocketAddrs;
-use trust_dns_resolver::Resolver;
+use trust_dns_resolver::{Resolver, Name};
 use trust_dns_resolver::config::{ResolverConfig, ResolverOpts, LookupIpStrategy};
+use trust_dns_client::udp::UdpClientConnection;
+use trust_dns_client::client::{SyncClient, Client};
+use trust_dns_resolver::proto::rr::{DNSClass, RecordType, Record, RData};
 
 fn main() {
     libstd();
     trust_dns_resolver();
+    txt();
 }
 
 // 名前解決は標準ライブラリで可能
 fn libstd() {
+    println!("/// libstd ///");
+
     // ポート番号を指定しないとエラーになる
     let host = "example.com:0";
     // let host = "localhost:0";
@@ -38,6 +45,8 @@ fn libstd() {
 // Can't create custom ResolverOpts · Issue #1004 · bluejekyll/trust-dns
 // https://github.com/bluejekyll/trust-dns/issues/1004
 fn trust_dns_resolver() {
+    println!("/// trust_dns_resolver ///");
+
     // FQDN(末尾にドット)を指定する
     let host = "example.com.";
 
@@ -51,4 +60,30 @@ fn trust_dns_resolver() {
     let address = response.iter().next().expect("no addresses returned!");
 
     println!("{:?}", address);
+}
+
+// 汎用的なクエリには trust-dns-client を使う
+// https://crates.io/crates/trust-dns-client
+fn txt() {
+    println!("/// txt ///");
+
+    let name_server = "8.8.8.8:53".parse().unwrap();
+    let conn = UdpClientConnection::new(name_server).unwrap();
+    let client = SyncClient::new(conn);
+
+    // TXTレコードを取得する
+    let name = Name::from_str("_dnsaddr.bootstrap.libp2p.io").unwrap();
+    let response = client.query(&name, DNSClass::IN, RecordType::TXT).unwrap();
+    // println!("{:?}", response);
+    let answers: &[Record] = response.answers();
+
+    for answer in answers.iter() {
+        if let &RData::TXT(ref txt) = answer.rdata() {
+            for d in txt.iter() {
+                println!("{}", std::str::from_utf8(d).unwrap());
+            }
+        } else {
+            assert!(false, "unexpected result");
+        }
+    }
 }
