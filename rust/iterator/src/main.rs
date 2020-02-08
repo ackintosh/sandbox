1911ebd0e0d0ee4d56438d051cfc67d7882c2625
akihito.nakano <sora.akatsuki@gmail.com>
2020-02-08 13:32:36 +0900

    Add flatten

--- rust/iterator/src/main.rs
+++ rust/iterator/src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
+    filter_map();
+    flatten();
+}
+
+fn filter_map() {
+    println!("/// filter_map ///");
     // filter_map
     // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.filter_map
     let data = ["foo".to_owned(), "bar".to_owned(), "baz".to_owned()];
@@ -13,4 +19,23 @@ fn main() {
     println!("{:?}", r);
     assert_eq!(Some("bar!!!".to_owned()), r.next());
     assert_eq!(None, r.next());
+}
+
+fn flatten() {
+    println!("/// flatten ///");
+    // flatten
+    // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.flatten
+    let iter = vec![vec![0, 1, 2], vec![3, 4]].into_iter();
+
+    println!("{:?}", iter.clone().flatten().collect::<Vec<_>>()); // [0, 1, 2, 3, 4]
+
+    // .flat_map(|x| x) と同等
+    assert_eq!(
+        iter.clone()
+            .flatten()
+            .collect::<Vec<_>>(),
+        iter.clone()
+            .flat_map(|x| x)
+            .collect::<Vec<_>>(),
+    );
 }
