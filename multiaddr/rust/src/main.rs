1c076de27b6293b41e7933f1ce1ce6b7348ef922
akihito.nakano <sora.akatsuki@gmail.com>
2020-02-14 22:15:13 +0900

    Update

--- multiaddr/rust/src/main.rs
+++ multiaddr/rust/src/main.rs
@@ -12,4 +12,20 @@ fn main() {
     addr2.iter().for_each(|protocol| addr.push(protocol));
     println!("{:?}", addr);
     // "/ip4/127.0.0.1/tcp/2000/udt/sctp/5678"
+
+    let addr3: Multiaddr = "".parse().unwrap(); // 空
+    println!("{:?}", addr3);
+    // ""
+
+    let addrs: Vec<Multiaddr> = vec![
+        "/ip4/127.0.0.1/tcp/2000".parse().unwrap(),
+        "/udt/sctp/5678".parse().unwrap()
+    ];
+
+    let concat = addrs.iter().fold(Multiaddr::empty(), |mut addr, next| {
+        next.iter().for_each(|protocol| addr.push(protocol));
+        addr
+    });
+    println!("{:?}", concat);
+    // "/ip4/127.0.0.1/tcp/2000/udt/sctp/5678"
 }
