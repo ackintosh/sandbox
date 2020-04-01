0747b1e333b5ac501c894496b2570c0c92889616
ackintosh <sora.akatsuki@gmail.com>
2020-04-01 18:21:13 +0900

    AppSync

---
+++ terraform/aws/appsync/main.tf
@@ -0,0 +1,51 @@
+/////////////////////////////////////////////////////////
+// API
+/////////////////////////////////////////////////////////
+// https://www.terraform.io/docs/providers/aws/r/appsync_graphql_api.html
+resource "aws_appsync_graphql_api" "example" {
+  authentication_type = "API_KEY"
+  name = "example"
+  schema = file("./schema.graphqls")
+}
+
+output "uri_http" {
+  value = aws_appsync_graphql_api.example.uris["GRAPHQL"]
+}
+output "uri_websocket" {
+  value = aws_appsync_graphql_api.example.uris["REALTIME"]
+}
+
+
+/////////////////////////////////////////////////////////
+// データソース
+/////////////////////////////////////////////////////////
+// https://www.terraform.io/docs/providers/aws/r/appsync_datasource.html
+resource "aws_appsync_datasource" "example_datasource" {
+  api_id = aws_appsync_graphql_api.example.id
+  name = "livedoor_weather_api"
+
+  // 外部API
+  type = "HTTP"
+  http_config {
+    endpoint = "http://weather.livedoor.com"
+  }
+}
+
+
+/////////////////////////////////////////////////////////
+// リゾルバ
+/////////////////////////////////////////////////////////
+// https://www.terraform.io/docs/providers/aws/r/appsync_resolver.html
+resource "aws_appsync_resolver" "example_resolver" {
+  api_id = aws_appsync_graphql_api.example.id
+  field = "weather"
+  type = "Query"
+
+  // データソース
+  // * idではなく `name` を指定
+  data_source = aws_appsync_datasource.example_datasource.name
+
+  // マッピングテンプレート
+  request_template = file("./resolver/request_template.vm")
+  response_template = file("./resolver/response_template.vm")
+}
