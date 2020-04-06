aae20227ff36aa4bc221e7a4e9cdca72140292bc
ackintosh <sora.akatsuki@gmail.com>
2020-04-06 13:13:02 +0900

    seeds for DynamoDB

---
+++ serverless-framework/seed-data/create_seed_data.js
@@ -0,0 +1,25 @@
+const faker = require('faker');
+const jsonfile = require('jsonfile');
+
+const numUsers = 10;
+const udata = [];
+
+faker.seed(1000);
+
+for (let i = 1; i <= numUsers; i++) {
+  const user = {
+    id: i,
+    name: faker.internet.userName(),
+  };
+  udata.push(user);
+}
+
+const ufile = 'generated/Users.json';
+
+jsonfile.writeFileSync(ufile, udata, function(err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('data created successfully');
+  }
+});
