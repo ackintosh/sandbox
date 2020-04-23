f7f143dc17d9e423d43eab3c16c322fe57c84a38
akihito.nakano <sora.akatsuki@gmail.com>
2020-04-23 22:56:33 +0900

    Add procon

---
+++ rust/procon/src/alds1_1_d_maximum_profit.rs
@@ -0,0 +1,45 @@
+// プロコンのためのアルゴリズムとデータ構造
+// 2.5 導入問題
+use std::convert::TryFrom;
+
+fn main(n: u64, r: Vec<u64>) -> i64 {
+    let ii = n.clone();
+    let jj = n.clone();
+
+    let mut max_profit: Option<i64> = None;
+
+    for i in 0..ii {
+        for j in (i+1..jj).rev() {
+            println!("i : {}", i);
+            println!("j : {}", j);
+
+            let rj = i64::try_from(
+                r[usize::try_from(j).unwrap()]
+            ).unwrap();
+            let ri = i64::try_from(
+                r[usize::try_from(i).unwrap()]
+            ).unwrap();
+            println!("rj : {}", rj);
+            println!("ri : {}", ri);
+
+            let profit = rj - ri;
+            println!("profit : {}", profit);
+
+            if max_profit.is_none() {
+                max_profit = Some(profit);
+            } else {
+                if profit > max_profit.unwrap() {
+                    max_profit = Some(profit);
+                }
+            }
+            println!("max_profit : {}", max_profit.unwrap());
+        }
+    }
+
+    max_profit.unwrap()
+}
+
+#[test]
+fn test() {
+    assert_eq!(main(3, vec![4, 3, 2]), -1);
+}
---
+++ rust/procon/src/lib.rs
@@ -0,0 +1,13 @@
+mod alds1_1_d_maximum_profit;
+
+fn foo() {
+    println!( "foo");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
