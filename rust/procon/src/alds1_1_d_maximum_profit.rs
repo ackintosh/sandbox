b7a0fb9c3f27f5a9b4a94bb12ea9c844605cc60a
akihito.nakano <sora.akatsuki@gmail.com>
2020-04-24 09:24:26 +0900

    Add O(n)

--- rust/procon/src/alds1_1_d_maximum_profit.rs
+++ rust/procon/src/alds1_1_d_maximum_profit.rs
@@ -2,6 +2,9 @@
 // 2.5 導入問題
 use std::convert::TryFrom;
 
+////////////////////
+// O(n^2)
+////////////////////
 fn main(n: u64, r: Vec<u64>) -> i64 {
     let ii = n.clone();
     let jj = n.clone();
@@ -39,8 +42,43 @@ fn main(n: u64, r: Vec<u64>) -> i64 {
     max_profit.unwrap()
 }
 
+////////////////////
+// O(n)
+////////////////////
+fn main2(n: u64, r: Vec<u64>) -> i64 {
+    let mut minv = r.first().unwrap().clone();
+    let mut max_profit: Option<i64> = None;
+
+    for j in 1..n {
+        let rj = r[usize::try_from(j).unwrap()];
+
+        // Update `max_profit`
+        let v = i64::try_from(rj.clone()).unwrap();
+        let v_min = i64::try_from(minv.clone()).unwrap();
+        let result: i64 = v - v_min;
+
+        if max_profit.is_none() {
+            max_profit = Some(result);
+        } else {
+            if result > max_profit.unwrap() {
+                max_profit = Some(result);
+            }
+        }
+
+        // Update `minv`
+        if rj < minv {
+            minv = rj;
+        }
+    }
+
+    max_profit.unwrap()
+}
+
 #[test]
 fn test() {
     assert_eq!(main(6, vec![5, 3, 1, 3, 4, 3]), 3);
     assert_eq!(main(3, vec![4, 3, 2]), -1);
+
+    assert_eq!(main2(6, vec![5, 3, 1, 3, 4, 3]), 3);
+    assert_eq!(main2(3, vec![4, 3, 2]), -1);
 }
