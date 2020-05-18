46b4427284d18c158ea2d91ef208b41818ad0a90
ackintosh <sora.akatsuki@gmail.com>
2020-05-18 21:47:27 +0900

    Add leveldb

---
+++ rust/level-db/src/lib.rs
@@ -0,0 +1,32 @@
+use tempdir::TempDir;
+use leveldb::database::kv::KV;
+use leveldb::database::options::{Options, WriteOptions, ReadOptions};
+use leveldb::database::Database;
+
+#[test]
+fn leveldb() {
+    let tempdir = TempDir::new("leveldb").unwrap();
+    let path = tempdir.path();
+
+    let mut options = Options::new();
+    options.create_if_missing = true;
+    let mut database = match Database::open(path, options) {
+        Ok(db) => db,
+        Err(e) => panic!("failed to open database: {:?}", e),
+    };
+
+    let write_options = WriteOptions::new();
+    match database.put(write_options, 1, &[1]) {
+        Ok(_) => (),
+        Err(e) => panic!("failed to write to database: {:?}", e),
+    };
+
+    let read_options = ReadOptions::new();
+    match database.get(read_options, 1) {
+        Ok(data) => {
+            assert!(data.is_some());
+            assert_eq!(data, Some(vec![1]));
+        }
+        Err(e) => panic!("failed reading data: {:?}", e)
+    }
+}
