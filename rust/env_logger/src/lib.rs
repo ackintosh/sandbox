e326d129a9f8d19e2b269a1a118a49e7582e779d
ackintosh <sora.akatsuki@gmail.com>
2020-07-13 18:24:20 +0900

    env_logger

---
+++ rust/env_logger/src/lib.rs
@@ -0,0 +1,42 @@
+// logクレートのマクロのログレベルを環境変数経由で設定できる
+#[macro_use]
+extern crate log;
+
+use env_logger::{Builder, Env};
+
+#[test]
+fn it_works() {
+    env_logger::init();
+
+    debug!("debugログ: {}", "xxxxx");
+    error!("errorログ: {}", "xxxxx");
+
+    // $ RUST_LOG=error cargo test
+    // で実行すると error ログだけが表示される
+    // [2020-07-13T09:11:51Z ERROR env_logger] errorログ: xxxxx
+}
+
+mod builder {
+    use env_logger::{Builder, Env};
+
+    #[test]
+    fn default() {
+        // デフォルトの環境変数 `RUST_LOG` から設定値を取る
+        let mut builder = Builder::from_env(Env::default());
+        builder.init();
+
+        debug!("builder: {}", "test");
+    }
+
+    #[test]
+    fn custom_env() {
+        // 環境変数 `CUSTOM_ENV` から設定値を取る
+        let mut builder = Builder::from_env(Env::new().filter("CUSTOM_ENV"));
+        builder.init();
+
+        debug!("custom_env: {}", "test");
+
+        // $ CUSTOM_ENV=debug cargo test builder::custom_env
+        // で実行すると環境変数が反映される
+    }
+}
