3ce1884aed0ec6e06a8f8490bd59f69c607ebfac
ackintosh <sora.akatsuki@gmail.com>
2020-09-04 17:37:55 +0900

    Update

--- lambda/golang/main.go
+++ lambda/golang/main.go
@@ -1,30 +1,38 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "time"
+	"context"
+	"fmt"
+	"strconv"
+	"time"
 
-    "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
-    for _, message := range sqsEvent.Records {
-        fmt.Printf("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
-        fmt.Printf("Attributes: %s \n", message.Attributes)
+	for _, message := range sqsEvent.Records {
+		fmt.Printf("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
+		fmt.Printf("Attributes: %s \n", message.Attributes)
 
-        // SentTimestamp: メッセージがキューに送られた時間（ミリ秒）
-        // https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_ReceiveMessage.html#API_ReceiveMessage_RequestParameters
-        fmt.Printf("SentTimestamp: %s \n", message.Attributes["SentTimestamp"])
+		// 現在（ミリ秒）
+		var now = time.Now().UnixNano() / int64(time.Millisecond)
+		fmt.Printf("The message has been handled at: %d \n", now)
 
-        // 現在（ミリ秒）
-        fmt.Printf("The message has been handled at: %d \n", time.Now().UnixNano() / int64(time.Millisecond))
-    }
+		// SentTimestamp: メッセージがキューに送られた時間（ミリ秒）
+		// https://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_ReceiveMessage.html#API_ReceiveMessage_RequestParameters
+		var sentTimestamp, err = strconv.ParseInt(message.Attributes["SentTimestamp"], 10, 64)
+		if err != nil {
+			panic("failed to convert SetTimestamp")
+		}
+		fmt.Printf("SentTimestamp: %d \n", sentTimestamp)
 
-    return nil
+		fmt.Printf("Elapsed time in millisecond: %d \n", now-sentTimestamp)
+	}
+
+	return nil
 }
 
 func main() {
-    lambda.Start(handler)
+	lambda.Start(handler)
 }
