81f2d30caebce2e2047c6c19078cf0cdabe3f05e
ackintosh <sora.akatsuki@gmail.com>
2020-09-21 09:11:14 +0900

    Add go

---
+++ go/dangling_pointer.go
@@ -0,0 +1,26 @@
+package main;
+
+import "fmt"
+
+// Handling Dangling Pointers in Go - Stack Overflow
+// https://stackoverflow.com/questions/46987513/handling-dangling-pointers-in-go
+func main() {
+	var user = newUser("testuser")
+	fmt.Print("test: %s", user.Name)
+}
+
+type User struct {
+	Name string
+}
+
+// newUserでは関数内で作ったUserインスタンスへのポインタを返しているが、
+// コンパイラが、関数のスコープを抜けた後もポインタが使われることを理解して、
+// インスタンスをヒープに配置する
+// なので、dangling pointerにならない
+
+// Effective go
+// https://golang.org/doc/effective_go.html#composite_literals
+// Note that, unlike in C, it's perfectly OK to return the address of a local variable; the storage associated with the variable survives after the function returns.
+func newUser(name string) *User {
+	return &User{ Name: name }
+}
---
+++ go/data_race.go
@@ -0,0 +1,7 @@
+package main
+
+// Data Race Detector - The Go Programming Language
+// https://golang.org/doc/articles/race_detector.html
+func main() {
+
+}
