4fe25b95aebb18d37dd2f583d97cb46d5a3b4732
ackintosh <sora.akatsuki@gmail.com>
2020-09-21 09:19:56 +0900

    data race

--- go/data_race.go
+++ go/data_race.go
@@ -1,7 +1,21 @@
 package main
 
+import "fmt"
+
 // Data Race Detector - The Go Programming Language
 // https://golang.org/doc/articles/race_detector.html
-func main() {
 
+// go run -race data_race.go
+func main() {
+	c := make(chan bool)
+	m := make(map[string]string)
+	go func() {
+		m["1"] = "a" // First conflicting access.
+		c <- true
+	}()
+	m["2"] = "b" // Second conflicting access.
+	<-c
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
