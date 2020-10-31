cd9c8b0d8462dba60beeb0cc6d693a4b2ff4d7e7
ackintosh <sora.akatsuki@gmail.com>
2020-10-31 22:43:48 +0900

    Vec::reverse()

--- rust/primitives/src/vector.rs
+++ rust/primitives/src/vector.rs
@@ -6,3 +6,25 @@ fn vector() {
     assert_eq!(vec[0usize], 1);
     assert_eq!(vec[2usize], 3);
 }
+
+#[test]
+fn reverse() {
+    let mut vec = vec![1, 2, 3];
+    vec.reverse();
+
+    // 順番が逆になる
+    assert_eq!(vec![3, 2, 1], vec);
+
+    // しかし pop() が返す要素の順番は変わらない
+    assert_eq!(1, vec.pop().unwrap());
+    assert_eq!(2, vec.pop().unwrap());
+    assert_eq!(3, vec.pop().unwrap());
+
+    let mut vec = vec![1, 2, 3];
+    vec.reverse();
+    // イテレータの順番は逆になる
+    let mut iter = vec.iter();
+    assert_eq!(&3, iter.next().unwrap());
+    assert_eq!(&2, iter.next().unwrap());
+    assert_eq!(&1, iter.next().unwrap());
+}
