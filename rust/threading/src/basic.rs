a35c5b5c424b5273497065cd019c4a29d8c4ecd6
ackintosh <sora.akatsuki@gmail.com>
2020-12-25 17:43:23 +0900

    threading

--- rust/threading/src/basic.rs
+++ rust/threading/src/basic.rs
@@ -6,3 +6,21 @@ fn test() {
 
     dbg!(handle.join());
 }
+
+// スレッドに変数を渡す
+#[test]
+fn move_variants() {
+    let mut handles = vec![];
+
+    for x in 0..10 {
+        // `move` で所有権をスレッドに移さないとコンパイルエラーになってしまう
+        //   -> スレッドの生存期間はわからないので、変数 x への参照を保証できない
+        handles.push(std::thread::spawn(move || {
+            println!("Hello, world!: {}", x);
+        }));
+    }
+
+    for h in handles {
+        let _ = h.join();
+    }
+}
