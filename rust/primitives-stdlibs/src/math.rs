d101606ff1f75f70bd06872fca0021fe4ace4328
ackintosh <sora.akatsuki@gmail.com>
2021-01-02 16:56:11 +0900

    update

--- rust/primitives-stdlibs/src/math.rs
+++ rust/primitives-stdlibs/src/math.rs
@@ -12,3 +12,57 @@ fn test() {
     assert_eq!(n, 1.5);
     println!("{:?}", n);
 }
+
+mod overflow {
+    use std::ops::Add;
+
+    #[test]
+    fn saturating() {
+        let n = 10u8;
+        assert_eq!(255, n.add(245));
+
+        // u8 の上限を超えるのでオーバーフローが起きてしまう
+        //
+        // エラーメッセージ:
+        // attempt to add with overflow
+        // thread 'math::saturating' panicked at 'attempt to add with overflow'
+        // let _ = n.add(246);
+
+        // saturating_add を使えば、上限で止まって、エラーにならない
+        let result = n.saturating_add(246);
+        assert_eq!(255, result);
+    }
+}
+
+mod underflow {
+    #[test]
+    fn saturating_sub() {
+        let ten = 10u32;
+
+        // let result = ten - 11;
+        // *下記エラーになる*
+        // =======================================================================
+        // attempt to subtract with overflow
+        // thread 'underflow' panicked at 'attempt to subtract with overflow',
+        // =======================================================================
+
+        // saturating_sub を使うことでunderflowを防げる
+        // https://doc.rust-lang.org/std/primitive.usize.html#method.saturating_sub
+        assert_eq!(
+            ten.saturating_sub(11),
+            0
+        );
+    }
+
+    #[test]
+    fn checked_sub() {
+        assert_eq!(
+            10u32.checked_sub(1),
+            Some(9)
+        );
+        assert_eq!(
+            10u32.checked_sub(100),
+            None
+        );
+    }
+}
