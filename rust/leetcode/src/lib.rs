20dad6bf6624261346a77eeae17ad30601f518a4
ackintosh <sora.akatsuki@gmail.com>
2021-01-11 12:11:26 +0900

    water_bottles

--- rust/leetcode/src/lib.rs
+++ rust/leetcode/src/lib.rs
@@ -1,3 +1,4 @@
 mod number_of_good_pairs;
 mod richest_customer_wealth;
 mod running_sum_of_1d_array;
+mod water_bottles;
---
+++ rust/leetcode/src/water_bottles.rs
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/water-bottles/
+
+struct Solution;
+
+impl Solution {
+    pub fn num_water_bottles(num_bottles: i32, num_exchange: i32) -> i32 {
+        assert!(num_exchange > 1);
+
+        // 飲んだボトルの数
+        let mut count = 0;
+
+        // 最初に渡される、満タンのボトルをすべて飲む
+        count += num_bottles;
+        let mut empty_bottles = num_bottles.clone();
+
+        loop {
+            let exchanged = empty_bottles / num_exchange;
+            if exchanged == 0 {
+                break;
+            }
+
+            // 交換しきれなかった余りの空ボトル
+            empty_bottles = empty_bottles % num_exchange;
+            // 交換したボトルを飲む
+            count += exchanged;
+            // 交換したボトルを飲んだので、空ボトルに追加する
+            empty_bottles += exchanged;
+        }
+
+        count
+    }
+}
+
+#[test]
+fn test() {
+    assert_eq!(13, Solution::num_water_bottles(9, 3));
+    assert_eq!(19, Solution::num_water_bottles(15, 4));
+}
