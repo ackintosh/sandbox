a6a03ab872df927bf006447f7c522506ff666a02
ackintosh <sora.akatsuki@gmail.com>
2021-01-13 09:42:12 +0900

    shuffle_string

--- rust/leetcode/src/lib.rs
+++ rust/leetcode/src/lib.rs
@@ -2,4 +2,5 @@ mod count_odd_numbers_in_an_interval_range;
 mod number_of_good_pairs;
 mod richest_customer_wealth;
 mod running_sum_of_1d_array;
+mod shuffle_string;
 mod water_bottles;
---
+++ rust/leetcode/src/shuffle_string.rs
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/shuffle-string/
+use std::convert::TryFrom;
+
+struct Solution;
+
+impl Solution {
+    pub fn restore_string(s: String, indices: Vec<i32>) -> String {
+        assert_eq!(s.len(), indices.len());
+
+        let mut data: Vec<(usize, &str)> = vec![];
+        for (pos, str) in s.split("").filter(|&s| !s.is_empty()).enumerate() {
+            let move_to = usize::try_from(indices[pos]).unwrap();
+            data.push((move_to, str));
+        }
+
+        // indicesの昇順にソートする
+        data.sort_by_key(|d| d.0);
+
+        let mut result = String::new();
+        for (_pos, str) in data.iter() {
+            result.push_str(str);
+        }
+
+        result
+    }
+}
+
+#[test]
+fn test() {
+    assert_eq!("leetcode".to_owned(), Solution::restore_string("codeleet".to_owned(), vec![4, 5, 6, 7, 0, 2, 1, 3]));
+}
