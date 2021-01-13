93afcfa2da1c3b9a851254ead86d069ec90672a3
ackintosh <sora.akatsuki@gmail.com>
2021-01-13 09:20:06 +0900

    split

--- rust/primitives-stdlibs/src/string_str.rs
+++ rust/primitives-stdlibs/src/string_str.rs
@@ -1,93 +1,130 @@
-use regex::Regex;
 
 /////////////////////////////////////////////////////////
 // str
 /////////////////////////////////////////////////////////
-#[test]
-fn str() {
-    let str = "1 2 + 3 4 - *";
+mod str {
+    use regex::Regex;
 
-    // Split構造体が返る
-    // https://doc.rust-lang.org/std/str/struct.Split.html
-    let split = str.split(" ");
-    println!("{:?}", split);
+    #[test]
+    fn str() {
+        let str = "1 2 + 3 4 - *";
 
-    // Split構造体がIteratorトレイトを実装している
-    let strs: Vec<&str> = str.split(" ").collect();
-    println!("{:?}", strs);
+        // Split構造体が返る
+        // https://doc.rust-lang.org/std/str/struct.Split.html
+        let split = str.split(" ");
+        println!("{:?}", split);
 
-    for s in str.split(" ").collect::<Vec<&str>>() {
-        print!("{} ", s)
+        // Split構造体がIteratorトレイトを実装している
+        let strs: Vec<&str> = str.split(" ").collect();
+        println!("{:?}", strs);
+
+        for s in str.split(" ").collect::<Vec<&str>>() {
+            print!("{} ", s)
+        }
+        print!("\n");
     }
-    print!("\n");
+
+    /////////////////////////////////////////////////////////
+    // 分割
+    /////////////////////////////////////////////////////////
+    #[test]
+    fn split() {
+        let str = "abc";
+        for s in str.split("") {
+            println!("split: {}", s);
+            // ※空文字で分割した場合、前後に空文字が入る
+            // split:
+            // split: a
+            // split: b
+            // split: c
+            // split:
+        }
+
+        for s in str.split("").skip_while(|&s| s.is_empty()) {
+            println!("Str::split() & Iterator::skip_while(): {}", s);
+            // ※skip_whileだと最後の空文字が残ってしまう
+            // Str::split() & Iterator::skip_while(): a
+            // Str::split() & Iterator::skip_while(): b
+            // Str::split() & Iterator::skip_while(): c
+            // Str::split() & Iterator::skip_while():
+        }
+
+        for s in str.split("").filter(|&s| !s.is_empty()) {
+            println!("Str::split() & Iterator::filter(): {}", s);
+            // ※前後の空文字が除かれて、3文字だけが出力される
+            // Str::split() & Iterator::filter(): a
+            // Str::split() & Iterator::filter(): b
+            // Str::split() & Iterator::filter(): c
+        }
+    }
+
+    // How can I split a string (String or &str) on more than one delimiter? - Stack Overflow
+    // https://stackoverflow.com/questions/29240157/how-can-i-split-a-string-string-or-str-on-more-than-one-delimiter
+    #[test]
+    fn split_with_regex() {
+        // r"" はraw string literal
+        // Rust: Raw string literals - rahul thakoor
+        // https://rahul-thakoor.github.io/rust-raw-string-literals/
+        let regex = Regex::new(r",|\.").unwrap();
+        let str = "aaa,bbb.ccc";
+
+        for s in regex.split(str) {
+            println!("{}", s);
+        }
+        // aaa
+        // bbb
+        // ccc
+
+        //////////////////////////////////
+        // 配列から正規表現を作るパターン
+        //////////////////////////////////
+        let keywords: [&str; 3] = [",", ".", "|"];
+        let reg_str = keywords.iter()
+            .map(|&k| {
+                if [".", "|"].contains(&k) {
+                    // エスケープが必要な文字
+                    format!("\\{}", k)
+                } else {
+                    String::from(k)
+                }
+            })
+            .collect::<Vec<_>>()
+            .join("|");
+        let regex = Regex::new(&reg_str).unwrap();
+        for s in regex.split("aaa,bbb.ccc|ddd") {
+            println!("{}", s);
+        }
+        // aaa
+        // bbb
+        // ccc
+        // ddd
+    }
 }
 
 /////////////////////////////////////////////////////////
 // String
 /////////////////////////////////////////////////////////
-#[test]
-fn remove_all_whitespace() {
-    // 文字列からスペースを削除する
-    let mut s = "a b c".to_owned();
-    s.retain(|c| !c.is_whitespace());
+mod string {
+    #[test]
+    fn remove_all_whitespace() {
+        // 文字列からスペースを削除する
+        let mut s = "a b c".to_owned();
+        s.retain(|c| !c.is_whitespace());
 
-    assert_eq!(s, "abc".to_owned());
-}
-
-#[test]
-fn string_pos() {
-    let mut s = "aaabbbccc".to_owned();
-
-    // bbb以降を切り落とす
-    if let Some(pos) = s.find("bbb") {
-        s.replace_range(pos.., "");
+        assert_eq!(s, "abc".to_owned());
     }
 
-    assert_eq!(s, "aaa".to_owned());
+    #[test]
+    fn string_pos() {
+        let mut s = "aaabbbccc".to_owned();
+
+        // bbb以降を切り落とす
+        if let Some(pos) = s.find("bbb") {
+            s.replace_range(pos.., "");
+        }
+
+        assert_eq!(s, "aaa".to_owned());
+    }
 }
 
 
-/////////////////////////////////////////////////////////
-// 分割
-/////////////////////////////////////////////////////////
-// How can I split a string (String or &str) on more than one delimiter? - Stack Overflow
-// https://stackoverflow.com/questions/29240157/how-can-i-split-a-string-string-or-str-on-more-than-one-delimiter
-#[test]
-fn split_with_regex() {
-    // r"" はraw string literal
-    // Rust: Raw string literals - rahul thakoor
-    // https://rahul-thakoor.github.io/rust-raw-string-literals/
-    let regex = Regex::new(r",|\.").unwrap();
-    let str = "aaa,bbb.ccc";
-
-    for s in regex.split(str) {
-        println!("{}", s);
-    }
-    // aaa
-    // bbb
-    // ccc
-
-    //////////////////////////////////
-    // 配列から正規表現を作るパターン
-    //////////////////////////////////
-    let keywords: [&str; 3] = [",", ".", "|"];
-    let reg_str = keywords.iter()
-        .map(|&k| {
-            if [".", "|"].contains(&k) {
-                // エスケープが必要な文字
-                format!("\\{}", k)
-            } else {
-                String::from(k)
-            }
-        })
-        .collect::<Vec<_>>()
-        .join("|");
-    let regex = Regex::new(&reg_str).unwrap();
-    for s in regex.split("aaa,bbb.ccc|ddd") {
-        println!("{}", s);
-    }
-    // aaa
-    // bbb
-    // ccc
-    // ddd
-}
