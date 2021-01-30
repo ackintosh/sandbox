381e13705aa6b3cf1006e5d5c8c7bd96fee2588e
ackintosh <sora.akatsuki@gmail.com>
2021-01-30 17:16:24 +0900

    update

--- rust/leetcode/src/sum_of_nodes_with_even_valued_grandparent.rs
+++ rust/leetcode/src/sum_of_nodes_with_even_valued_grandparent.rs
@@ -26,73 +26,28 @@ struct Solution;
 
 impl Solution {
     pub fn sum_even_grandparent(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        if root.is_none() {
-            return 0;
-        }
-
-        let root_n = Self::sum_grandson(root.as_ref());
-
-        let root_node = root.unwrap();
-        let root_node_refcell: &RefCell<TreeNode> = root_node.borrow();
-
-        let left_n = root_node_refcell
-            .borrow()
-            .left
-            .as_ref()
-            .map_or(0, |left| Self::sum_even_grandparent(Some(left.clone())));
-        let right_n = root_node_refcell
-            .borrow()
-            .right
-            .as_ref()
-            .map_or(0, |right| Self::sum_even_grandparent(Some(right.clone())));
-
-        root_n
-            .checked_add(left_n)
-            .unwrap()
-            .checked_add(right_n)
-            .unwrap()
+        Self::solution(root, 1, 1)
     }
 
-    fn sum_grandson(root: Option<&Rc<RefCell<TreeNode>>>) -> i32 {
-        if root.is_none() {
-            return 0;
+    fn solution(
+        node: Option<Rc<RefCell<TreeNode>>>,
+        parent_val: i32,
+        grand_parent_val: i32,
+    ) -> i32 {
+        if let Some(node) = node {
+            let mut result = 0;
+            let node_refcell: &RefCell<TreeNode> = node.borrow();
+            let node_val = node_refcell.borrow().val;
+
+            if grand_parent_val % 2 == 0 {
+                result += node_val;
+            }
+
+            result += Self::solution(node_refcell.borrow().left.clone(), node_val, parent_val);
+            result += Self::solution(node_refcell.borrow().right.clone(), node_val, parent_val);
+            return result;
         }
-
-        let root_rc = root.unwrap();
-
-        let root_node: &RefCell<TreeNode> = root_rc.borrow();
-
-        if root_node.borrow().val % 2 != 0 {
-            return 0;
-        }
-
-        let left_val = root_node.borrow().left.as_ref().map_or(0, |left| {
-            let left_node: &RefCell<TreeNode> = left.borrow();
-            let leftleft_val = left_node.borrow().left.as_ref().map_or(0, |leftleft| {
-                let leftleft_node: &RefCell<TreeNode> = leftleft.borrow();
-                leftleft_node.borrow().val
-            });
-            let leftright_val = left_node.borrow().right.as_ref().map_or(0, |leftright| {
-                let leftright_node: &RefCell<TreeNode> = leftright.borrow();
-                leftright_node.borrow().val
-            });
-            leftleft_val.checked_add(leftright_val).unwrap()
-        });
-
-        let right_val = root_node.borrow().right.as_ref().map_or(0, |right| {
-            let right_node: &RefCell<TreeNode> = right.borrow();
-            let rightleft_val = right_node.borrow().left.as_ref().map_or(0, |rightleft| {
-                let rightleft_node: &RefCell<TreeNode> = rightleft.borrow();
-                rightleft_node.borrow().val
-            });
-            let rightright_val = right_node.borrow().right.as_ref().map_or(0, |rightright| {
-                let rightright_node: &RefCell<TreeNode> = rightright.borrow();
-                rightright_node.borrow().val
-            });
-            rightleft_val.checked_add(rightright_val).unwrap()
-        });
-
-        left_val.checked_add(right_val).unwrap()
+        0
     }
 }
 
