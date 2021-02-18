385202a7aa89c456b478de84594cb6bc30f7cc8b
ackintosh <sora.akatsuki@gmail.com>
2021-02-18 13:36:46 +0900

    update

--- postgresql/initdb.sh
+++ postgresql/initdb.sh
@@ -4,5 +4,5 @@ set -Eeuo pipefail
 
 for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
   echo "Creating my_schema schema for $db"
-  echo "CREATE SCHEMA my_schema;" | psql --username "$POSTGRES_USER" $db -f -
+  echo "CREATE SCHEMA my_schema;" | psql -h localhost --username "$POSTGRES_USER" $db -f -
 done
