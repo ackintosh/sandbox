81a6c7c74533ea9e01c53026247379ac9bb619b6
ackintosh <sora.akatsuki@gmail.com>
2021-05-01 16:32:31 +0900

    maximum_product_of_two_elements_in_an_array

---
+++ rust/leetcode/src/maximum_product_of_two_elements_in_an_array.rs
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+
+struct Solution;
+
+impl Solution {
+    pub fn max_product(nums: Vec<i32>) -> i32 {
+        let mut ns = nums.clone();
+        ns.sort_unstable();
+
+        let a = ns.pop().unwrap() - 1;
+        let b = ns.pop().unwrap() - 1;
+
+        a * b
+    }
+}
+
+#[test]
+fn example1() {
+    assert_eq!(12, Solution::max_product(vec![3, 4, 5, 2]));
+}
+
+#[test]
+fn example2() {
+    assert_eq!(16, Solution::max_product(vec![1, 5, 4, 5]));
+}
+
+#[test]
+fn example3() {
+    assert_eq!(12, Solution::max_product(vec![3, 7]));
+}
