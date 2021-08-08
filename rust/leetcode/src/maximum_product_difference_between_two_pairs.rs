2ecac4fc1a83af5bc4d5532fdb95b29b34c88c80
akihito.nakano <sora.akatsuki@gmail.com>
2021-08-08 21:59:30 +0900

    maximum_product_difference_between_two_pairs

---
+++ rust/leetcode/src/maximum_product_difference_between_two_pairs.rs
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+
+struct Solution;
+
+impl Solution {
+    pub fn max_product_difference(nums: Vec<i32>) -> i32 {
+        let mut sorted = nums.clone();
+        sorted.sort_unstable();
+
+        sorted[sorted.len() - 1] * sorted[sorted.len() - 2] - sorted[0] * sorted[1]
+    }
+}
+
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn example1() {
+        assert_eq!(34, Solution::max_product_difference(vec![5, 6, 2, 7, 4]));
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(
+            64,
+            Solution::max_product_difference(vec![4, 2, 5, 9, 7, 4, 8])
+        );
+    }
+}
