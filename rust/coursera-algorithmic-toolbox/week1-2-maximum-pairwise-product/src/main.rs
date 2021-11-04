9da2e06d668bbe5226331283a24abebbfbb3e024
akihito.nakano <sora.akatsuki@gmail.com>
2021-11-05 01:59:07 +0900

    fix overflow

--- rust/coursera-algorithmic-toolbox/week1-2-maximum-pairwise-product/src/main.rs
+++ rust/coursera-algorithmic-toolbox/week1-2-maximum-pairwise-product/src/main.rs
@@ -5,25 +5,28 @@ fn main() {
     let n = {
         let mut buff = String::new();
         std::io::stdin().read_line(&mut buff).unwrap();
-        i32::from_str(&buff.trim()).unwrap()
+        u64::from_str(&buff.trim()).unwrap()
     };
 
     let nums = {
         let mut buff = String::new();
         std::io::stdin().read_line(&mut buff).unwrap();
         let words = buff.split_whitespace();
-        words.map(|w| i32::from_str(w).unwrap()).collect::<Vec<_>>()
+        words.map(|w| u64::from_str(w).unwrap()).collect::<Vec<_>>()
     };
 
     println!("{}", naive_algorithm(n, nums));
 }
 
-fn naive_algorithm(n: i32, nums: Vec<i32>) -> i32 {
+fn naive_algorithm(n: u64, nums: Vec<u64>) -> u64 {
     let mut result = 0;
     for i in 0..n {
         for j in (i + 1)..n {
-            result =
-                result.max(nums[usize::try_from(i).unwrap()] * nums[usize::try_from(j).unwrap()]);
+            result = result.max(
+                nums[usize::try_from(i).unwrap()]
+                    .checked_mul(nums[usize::try_from(j).unwrap()])
+                    .unwrap(),
+            );
         }
     }
 
@@ -39,4 +42,6 @@ fn test_naive_algorithm() {
         140,
         naive_algorithm(10, vec![7, 5, 14, 2, 8, 8, 10, 1, 2, 3])
     );
+
+    assert_eq!(9000000000, naive_algorithm(2, vec![100000, 90000]));
 }
