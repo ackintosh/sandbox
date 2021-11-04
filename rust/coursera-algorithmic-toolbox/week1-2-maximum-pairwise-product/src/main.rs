2c107c906525517a7d7936a88bb85921a49757e8
akihito.nakano <sora.akatsuki@gmail.com>
2021-11-05 02:43:26 +0900

    fast algorithm

--- rust/coursera-algorithmic-toolbox/week1-2-maximum-pairwise-product/src/main.rs
+++ rust/coursera-algorithmic-toolbox/week1-2-maximum-pairwise-product/src/main.rs
@@ -33,6 +33,38 @@ fn naive_algorithm(n: u64, nums: Vec<u64>) -> u64 {
     result
 }
 
+fn fast_algorithm(n: u64, nums: Vec<u64>) -> u64 {
+    let largest_index = {
+        let mut index = 0_usize;
+        let mut number = 0;
+        for i in 0..usize::try_from(n).unwrap() {
+            if nums[i] > number {
+                index = i;
+                number = nums[i];
+            }
+        }
+
+        index
+    };
+
+    let second_largest_index = {
+        let mut index = 0_usize;
+        let mut number = 0;
+        for i in 1..usize::try_from(n).unwrap() {
+            if i != largest_index && nums[i] > number {
+                index = i;
+                number = nums[i];
+            }
+        }
+
+        index
+    };
+
+    nums[largest_index]
+        .checked_mul(nums[second_largest_index])
+        .unwrap()
+}
+
 #[test]
 fn test_naive_algorithm() {
     // sample1
@@ -44,4 +76,28 @@ fn test_naive_algorithm() {
     );
 
     assert_eq!(9000000000, naive_algorithm(2, vec![100000, 90000]));
+}
+
+#[test]
+fn test_fast_algorithm() {
+    // sample1
+    assert_eq!(6, fast_algorithm(3, vec![1, 2, 3]));
+    // sample2
+    assert_eq!(
+        140,
+        fast_algorithm(10, vec![7, 5, 14, 2, 8, 8, 10, 1, 2, 3])
+    );
+
+    assert_eq!(9000000000, fast_algorithm(2, vec![100000, 90000]));
+}
+
+#[test]
+fn test_fast_algorithm_on_large_data_sets() {
+    let n: u64 = 2_u64 * 10_u64.checked_pow(5).unwrap();
+    let mut nums = vec![];
+    for i in 1..=n {
+        nums.push(i);
+    }
+
+    assert_eq!(39999800000, fast_algorithm(n, nums));
 }
