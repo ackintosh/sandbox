508a1ccd7926b70b12b8ace09806843312886b3c
akihito.nakano <sora.akatsuki@gmail.com>
2021-12-05 14:34:27 +0900

    Tweak

--- rust/algorithms/src/algorithms/binary_search.rs
+++ rust/algorithms/src/algorithms/binary_search.rs
@@ -3,19 +3,23 @@
 
 use std::cmp::Ordering;
 
+fn binary_search(input: &Vec<u8>, target: u8) -> Option<usize> {
+    _binary_search(input, target, 0, input.len() - 1)
+}
+
 // input: 探索対象のベクタ。昇順にソート済み
 // target: 探索する数値
 // start: ベクタの探索範囲の開始位置
 // end: ベクタの探索範囲の修了位置
-fn binary_search(input: &Vec<u8>, target: u8, start: usize, end: usize) -> Option<usize> {
+fn _binary_search(input: &Vec<u8>, target: u8, start: usize, end: usize) -> Option<usize> {
     if start > end {
         return None;
     }
     let m = (start + end) / 2;
     match input[m].cmp(&target) {
-        Ordering::Less => binary_search(input, target, m + 1, end),
+        Ordering::Less => _binary_search(input, target, m + 1, end),
         Ordering::Equal => Some(m),
-        Ordering::Greater => binary_search(input, target, start, end - 1),
+        Ordering::Greater => _binary_search(input, target, start, end - 1),
     }
 }
 
@@ -23,15 +27,15 @@ fn binary_search(input: &Vec<u8>, target: u8, start: usize, end: usize) -> Optio
 fn test_binary_search() {
     {
         let input = vec![1, 2];
-        assert_eq!(Some(0), binary_search(&input, 1, 0, 1));
-        assert_eq!(Some(1), binary_search(&input, 2, 0, 1));
-        assert_eq!(None, binary_search(&input, 3, 0, 1));
+        assert_eq!(Some(0), binary_search(&input, 1));
+        assert_eq!(Some(1), binary_search(&input, 2));
+        assert_eq!(None, binary_search(&input, 3));
     }
     {
         let input = vec![1, 2, 3];
-        assert_eq!(Some(0), binary_search(&input, 1, 0, 2));
-        assert_eq!(Some(1), binary_search(&input, 2, 0, 2));
-        assert_eq!(Some(2), binary_search(&input, 3, 0, 2));
-        assert_eq!(None, binary_search(&input, 4, 0, 2));
+        assert_eq!(Some(0), binary_search(&input, 1));
+        assert_eq!(Some(1), binary_search(&input, 2));
+        assert_eq!(Some(2), binary_search(&input, 3));
+        assert_eq!(None, binary_search(&input, 4));
     }
 }
