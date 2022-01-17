04bdce73e34d185adc4e956b4dbf672e405d4148
akihito.nakano <sora.akatsuki@gmail.com>
2022-01-18 08:07:35 +0900

    bash

---
+++ bash/kill.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# #################################################
+# プロセスが存在すれば kill コマンドで終了させる
+# #################################################
+PID=48937
+
+# 下記のように $? でステータスコードを確認するのは良くない
+# ps -p $PID > /dev/null
+# if [ $? -eq 0 ]; then
+# 参考:
+# https://github.com/koalaman/shellcheck/wiki/SC2181
+
+if ps -p $PID > /dev/null; then
+	kill $PID
+	echo "killed $PID."
+else
+	echo "PID $PID is already terminated."
+fi
+
