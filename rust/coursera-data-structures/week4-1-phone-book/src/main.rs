ff64ef411318fe0c8ed4d4da0cfc4a2e177e7330
akihito.nakano <sora.akatsuki@gmail.com>
2022-02-01 10:40:30 +0900

    week4-1

---
+++ rust/coursera-data-structures/week4-1-phone-book/src/main.rs
@@ -0,0 +1,140 @@
+use std::convert::TryFrom;
+
+fn main() {
+    let n = read_num();
+    let operations = read_operations(n);
+    // println!("{:?}", operations);
+
+    let mut phone_book = PhoneBook::new();
+
+    for operation in operations {
+        match operation.first().expect("should have element").as_str() {
+            "add" => {
+                let number = operation[1].parse::<u32>().unwrap();
+                phone_book.add(number, operation[2].to_string());
+            }
+            "find" => {
+                let number = operation[1].parse::<u32>().unwrap();
+                let name = phone_book.find(number);
+                if name.is_empty() {
+                    println!("not found");
+                } else {
+                    println!("{}", name);
+                }
+            }
+            "del" => {
+                let number = operation[1].parse::<u32>().unwrap();
+                phone_book.del(number);
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn read_num() -> u64 {
+    let mut buff = String::new();
+    std::io::stdin().read_line(&mut buff).unwrap();
+    buff.trim().parse::<u64>().unwrap()
+}
+
+fn read_operations(number_of_elements: u64) -> Vec<Vec<String>> {
+    let mut operations: Vec<Vec<String>> = vec![];
+
+    for _ in 0..number_of_elements {
+        let mut buff = String::new();
+        std::io::stdin().read_line(&mut buff).unwrap();
+
+        let mut strs = buff
+            .split_whitespace()
+            .map(|s| String::from(s))
+            .collect::<Vec<_>>();
+        operations.push(strs);
+    }
+
+    operations
+}
+
+// All phone numbers consist of decimal digits, they don’t have leading zeros, and each of them has no more than 7 digits.
+// All names are non-empty strings of latin letters, and each of them has length at most 15.
+struct PhoneBook {
+    cells: Vec<String>,
+}
+
+impl PhoneBook {
+    fn new() -> Self {
+        Self {
+            cells: vec!["".to_string(); 10000000],
+        }
+    }
+
+    fn add(&mut self, number: u32, name: String) {
+        self.cells[usize::try_from(number).unwrap()] = name;
+    }
+
+    fn find(&self, number: u32) -> String {
+        self.cells[usize::try_from(number).unwrap()].clone()
+    }
+
+    fn del(&mut self, number: u32) {
+        self.cells[usize::try_from(number).unwrap()] = "".to_string();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::PhoneBook;
+
+    #[test]
+    fn operations() {
+        let mut p = PhoneBook::new();
+        p.add(1, "test".into());
+
+        assert_eq!("test".to_string(), p.find(1));
+        assert_eq!("".to_string(), p.find(2));
+
+        p.del(1);
+        assert_eq!("".to_string(), p.find(1));
+    }
+
+    #[test]
+    fn phone_book() {
+        // Sample 1
+        {
+            let mut p = PhoneBook::new();
+            p.add(911, "police".to_string());
+            p.add(76213, "Mom".to_string());
+            p.add(17239, "Bob".to_string());
+
+            assert_eq!("Mom".to_string(), p.find(76213));
+            assert_eq!("".to_string(), p.find(910));
+            assert_eq!("police".to_string(), p.find(911));
+
+            p.del(910);
+            p.del(911);
+
+            assert_eq!("".to_string(), p.find(911));
+            assert_eq!("Mom".to_string(), p.find(76213));
+
+            p.add(76213, "daddy".to_string());
+
+            assert_eq!("daddy".to_string(), p.find(76213));
+        }
+
+        // Sample 2
+        {
+            let mut p = PhoneBook::new();
+            assert_eq!("".to_string(), p.find(3839442));
+
+            p.add(123456, "me".to_string());
+            p.add(0, "granny".to_string());
+
+            assert_eq!("granny".to_string(), p.find(0));
+            assert_eq!("me".to_string(), p.find(123456));
+
+            p.del(0);
+            p.del(0);
+
+            assert_eq!("".to_string(), p.find(0));
+        }
+    }
+}
--- rust/primitives-stdlibs/src/array.rs
+++ rust/primitives-stdlibs/src/array.rs
@@ -19,6 +19,54 @@ fn array() {
     //      let arr = vec![0; length];
 }
 
+// /////////////////////////////
+// 文字列の配列
+// /////////////////////////////
+#[test]
+fn array_string() {
+    // /////////////////////////////
+    // 参考: https://stackoverflow.com/questions/44186660/initializing-an-array-of-strings-in-rust
+    // /////////////////////////////
+
+    {
+        // 長さ 32 までの配列なら Default で初期化できる
+        //   -> 32までは手動で Default が実装されている
+        let array_string: [String; 32] = Default::default();
+        println!("{:?}", array_string);
+    }
+
+    {
+        // 長さ 32 以上の文字列の配列を初期化する場合
+        const EMPTY_STRING: String = String::new();
+        let array_string: [String; 100] = [EMPTY_STRING; 100];
+        println!("{:?}", array_string);
+    }
+}
+
+// /////////////////////////////
+// サイズが大きい配列
+// /////////////////////////////
+#[test]
+fn large_array() {
+    // ///////////////////////
+    // 参考: https://stackoverflow.com/questions/60777426/rust-large-array-thread-main-has-overflowed-its-stack?noredirect=1&lq=1
+    // ///////////////////////
+
+    // サイズが大きい配列を作ろうとするとスタックオーバーフローが起きる
+    // let array = [0; 1000000];
+    // println!("{:?}", array);
+    // エラーメッセージ:
+    //   thread 'array::large_array' has overflowed its stack
+    //   fatal runtime error: stack overflow
+
+    // 代わりに vector を使うことでヒープに領域を確保することになるので解決する
+    let array = vec![0; 1000000];
+    println!("{:?}", array);
+}
+
+// ///////////////////////
+// 多次元配列
+// ///////////////////////
 #[test]
 fn multidimensional_array() {
     {
