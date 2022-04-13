59896d0b8741137cbf5c6e4093b6be1bdd72963d
ackintosh <sora.akatsuki@gmail.com>
2022-04-13 15:21:40 +0900

    vars

--- rust/primitives-stdlibs/src/env.rs
+++ rust/primitives-stdlibs/src/env.rs
@@ -17,4 +17,9 @@ fn env() {
     // https://github.com/sigp/lighthouse/blob/6d4af4c9cac2b8d3aed94bd75305f9f9a5d6a326/common/eth2_config/src/lib.rs#L79
     let path = std::env::var("PATH").expect("should have $PATH environment variable");
     println!("{path}");
+
+    for (key, value) in std::env::vars() {
+        println!("key: {}, value: {}", key, value);
+    }
+
 }
