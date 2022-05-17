dd0973bcc25879f0157226f10aba5615748a29a3
ackintosh <sora.akatsuki@gmail.com>
2022-05-17 14:05:11 +0900

    for loop

---
+++ bash/for_loop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# How To Unix For Loop 1 to 100 Numbers - nixCraft
+# https://www.cyberciti.biz/faq/unix-for-loop-1-to-10/
+
+# Bash For Loop 1 to 100 Numbers
+for ((i=1;i<=100;i++)); 
+do 
+   # your-unix-command-here
+   echo $i
+done
