317d255db2ef42c3e2cd409ae5ae2767a96816cd
ackintosh <sora.akatsuki@gmail.com>
2022-08-11 17:36:03 +0900

    criterion

--- rust/crate-criterion/benches/my_benchmark.rs
+++ rust/crate-criterion/benches/my_benchmark.rs
@@ -1,6 +1,9 @@
 use crate_criterion::fibonacci;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+// 実行方法
+// $ cargo bench
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
 }
