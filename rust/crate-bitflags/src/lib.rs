88b75edfbe89ddae4c2efcba16780026235e6c88
ackintosh <sora.akatsuki@gmail.com>
2022-09-13 06:59:32 +0900

    bitflags

---
+++ rust/crate-bitflags/src/lib.rs
@@ -0,0 +1,21 @@
+#[cfg(test)]
+mod tests {
+    use bitflags::bitflags;
+
+    bitflags! {
+        struct Flags: u32 {
+            const A = 0b001;
+            const B = 0b010;
+            const C = 0b100;
+            const ABC = Self::A.bits | Self::B.bits | Self::C.bits;
+        }
+    }
+
+    #[test]
+    fn it_works() {
+        let e1 = Flags::A | Flags::C;
+        let e2 = Flags::B | Flags::C;
+        // union
+        assert_eq!((e1 | e2), Flags::ABC);
+    }
+}
