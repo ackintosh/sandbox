616240d594b3172746eceeccc232c220636824cd
ackintosh <sora.akatsuki@gmail.com>
2022-10-06 08:06:43 +0900

    chrono

--- rust/crate-chrono/src/lib.rs
+++ rust/crate-chrono/src/lib.rs
@@ -19,10 +19,18 @@ mod tests {
 
     #[test]
     fn parse() {
+        // ////////////////////////////
         // https://rust-lang-nursery.github.io/rust-cookbook/datetime/parse.html#parse-string-into-datetime-struct
+        // ////////////////////////////
+
         let no_timezone =
             NaiveDateTime::parse_from_str("2015-09-05 23:56:04", "%Y-%m-%d %H:%M:%S").unwrap();
         println!("{:?}", no_timezone);
         // Ok(2015-09-05T23:56:04)
+
+        let rfc3339 = DateTime::parse_from_rfc3339("1996-12-19T16:39:57-08:00").unwrap();
+        println!("{:?}", rfc3339);
+        let rfc3339 = DateTime::parse_from_rfc3339("2022-10-06T08:02:34+09:00").unwrap();
+        println!("{:?}", rfc3339);
     }
 }
