71c8887c32191951c28dd4cd74a83e9c18a7eb0f
ackintosh <sora.akatsuki@gmail.com>
2023-03-23 12:40:05 +0000

    mininet

---
+++ mininet/simple.py
@@ -0,0 +1,28 @@
+from mininet.net import Mininet
+from mininet.node import Controller, OVSSwitch
+from mininet.cli import CLI
+
+net = Mininet(controller=Controller, switch=OVSSwitch)
+
+c0 = net.addController('c0')
+
+s1 = net.addSwitch('s1')
+
+h1 = net.addHost('h1')
+h2 = net.addHost('h2')
+
+net.addLink(h1, s1)
+net.addLink(h2, s1)
+
+# ネットワークを開始
+net.start()
+
+# すべてのホスト間で ping コマンドを実行
+net.pingAll()
+
+# インタラクティブなCLIを提供
+# CLI(net)
+
+# ネットワークを停止
+net.stop()
+
