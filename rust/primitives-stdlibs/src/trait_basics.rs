9f46031f148b8cf7dcab571050017f3fed461e21
ackintosh <sora.akatsuki@gmail.com>
2023-04-10 09:11:50 +0900

    ipv6

--- rust/primitives-stdlibs/src/trait_basics.rs
+++ rust/primitives-stdlibs/src/trait_basics.rs
@@ -1,17 +1,25 @@
 use std::fmt::Debug;
 
 // Composite trait for a request id.
-pub trait ReqId: Debug + Copy + Clone {}
-impl<T> ReqId for T where T: Debug + Copy + Clone {}
+pub trait ReqId: Debug + Copy {}
+impl<T> ReqId for T where T: Debug + Copy {}
 
 #[derive(Debug, Copy, Clone)]
 struct Foo;
 
+#[derive(Debug, Copy, Clone)]
+enum Bar {
+    A,
+    B,
+}
+
 #[test]
 fn test() {
     let s = Foo;
-    // Foo は Debug, Copy, Clone を実装しているので引数に渡せる
+    // Foo や Bar は Debug, Copy を実装しているので引数に渡せる
     foo(s);
+    foo(Bar::A);
+    foo(Bar::B);
 }
 
 fn foo<T: ReqId>(req: T) {
