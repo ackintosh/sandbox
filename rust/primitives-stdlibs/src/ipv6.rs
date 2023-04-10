803d6af20c1555375d28c474f084f6efb137e742
ackintosh <sora.akatsuki@gmail.com>
2023-04-10 09:16:17 +0900

    ipv6

--- rust/primitives-stdlibs/src/ipv6.rs
+++ rust/primitives-stdlibs/src/ipv6.rs
@@ -5,9 +5,17 @@ use std::net::TcpListener;
 // IPv6が有効かどうかを判定する
 #[test]
 fn test_ipv6() {
-    let r = TcpListener::bind("[::]:0");
-    println!("{:?}", r);
-    drop(r);
+    let r = TcpListener::bind("[::1]:0");
+    println!("result: {:?}", r);
+    match r {
+        Ok(listener) => {
+            let a = listener.local_addr().unwrap();
+            println!("socketaddr: {:?}", a);
+        }
+        Err(_) => {
+            println!("ipv6 isn't available");
+        }
+    }
 
     // ローカルで何かデーモンプログラムが起動していないと判定できないかもしれない
     // if let Ok(_) = TcpStream::connect((IpAddr::V6(Ipv6Addr::LOCALHOST), 0)) {
