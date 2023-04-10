35108ded5a641415a974b8959e7a0b76c8d65f6a
ackintosh <sora.akatsuki@gmail.com>
2023-04-11 08:29:12 +0900

    ipv6

--- rust/crate-socket2/src/lib.rs
+++ rust/crate-socket2/src/lib.rs
@@ -1,9 +1,8 @@
 use std::net::SocketAddrV6;
 use socket2::{Domain, Protocol, Socket, Type};
 
-// cargo test --package crate-socket2 --lib test_ipv6 -- --show-output
 #[test]
-fn test_ipv6() {
+fn ipv6() {
     let socket = Socket::new(Domain::IPV6, Type::DGRAM, Some(Protocol::UDP)).unwrap();
     let r = socket.set_only_v6(true);
     println!("Socket::new : {:?}", r);
@@ -11,6 +10,4 @@ fn test_ipv6() {
     let addr = "[::]:0".parse::<SocketAddrV6>().unwrap();
     let r = socket.bind(&addr.into());
     println!("Socket::bind : {:?}", r);
-    let ad = socket.local_addr();
-    println!("local_addr: {:?}", ad.unwrap().as_socket_ipv6());
 }
