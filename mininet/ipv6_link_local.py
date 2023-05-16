b4b3595d8369c5162199b606f98e591a8cbdd3fc
ackintosh <sora.akatsuki@gmail.com>
2023-05-16 09:21:50 +0900

    link local

---
+++ mininet/ipv6_link_local.py
@@ -0,0 +1,32 @@
+from mininet.net import Mininet
+from mininet.node import Controller, OVSKernelSwitch
+from mininet.cli import CLI
+
+def ipv6Net():
+    net = Mininet(controller=Controller, switch=OVSKernelSwitch, build=False)
+
+    c0 = net.addController('c0')
+
+    s1 = net.addSwitch('s1')
+
+    h1 = net.addHost('h1')
+    h2 = net.addHost('h2')
+
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+
+    net.build()
+
+    # ネットワークを開始
+    net.start()
+
+    h1.cmd('ip -6 addr add fe80::1/64 dev h1-eth0')
+    h2.cmd('ip -6 addr add fe80::2/64 dev h2-eth0')
+
+    CLI(net)
+
+    net.stop()
+
+if __name__ == '__main__':
+    # setLogLevel('info')
+    ipv6Net()
