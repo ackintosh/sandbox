a5d7b14ec7c88998547d4374afdaaa2be7ba5e9b
ackintosh <sora.akatsuki@gmail.com>
2025-04-01 07:47:04 +0900

    mac

--- rust/crate-logroller/src/lib.rs
+++ rust/crate-logroller/src/lib.rs
@@ -1,20 +1,21 @@
 
 #[cfg(test)]
 mod tests {
-    use std::fs::{set_permissions, Permissions};
+    // use std::fs::{set_permissions, Permissions};
     use std::io::Write;
-    use std::os::unix::fs::PermissionsExt;
-    use std::path::Path;
+    // use std::os::unix::fs::PermissionsExt;
+    // use std::path::Path;
     use logroller::{LogRollerBuilder, Rotation, RotationSize};
 
     #[test]
     fn it_works() {
-        let path = Path::new("./logs/logger.log");
-        set_permissions(path, Permissions::from_mode(0o600)).unwrap();
+        // let path = Path::new("./logs/logger.log");
+        // set_permissions(path, Permissions::from_mode(0o600)).unwrap();
         
         let mut logger = LogRollerBuilder::new("./logs", "logger.log")
             .rotation(Rotation::SizeBased(RotationSize::Bytes(50)))
             .max_keep_files(3)
+            // .file_mode(0o600)
             .build().unwrap();
 
         writeln!(logger, "This is an info message").unwrap();
