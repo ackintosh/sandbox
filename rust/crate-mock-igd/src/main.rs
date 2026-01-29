1ab4b20c7cf6fe315d073cb6fb6bdce6e29c0c6e
ackintosh <sora.akatsuki@gmail.com>
2026-01-30 06:17:50 +0900

    mock-igd

--- rust/crate-mock-igd/src/main.rs
+++ rust/crate-mock-igd/src/main.rs
@@ -2,6 +2,9 @@ use mock_igd::{Action, MockIgdServer, Protocol, Responder};
 
 #[tokio::main]
 async fn main() {
+    let _ = tracing_subscriber::fmt::try_init();
+    tracing::info!("Hello, mock-igd!");
+
     // Start the mock IGD server
     let server = MockIgdServer::builder()
         // Enable SSDP
@@ -24,26 +27,26 @@ async fn main() {
     server
         .mock(
             Action::add_port_mapping()
-                .with_external_port(8080)
+                // .with_external_port(8080)
                 .with_protocol(Protocol::TCP),
             Responder::success(),
         )
         .await;
 
-    server
-        .mock(
-            Action::add_port_mapping().with_external_port(80),
-            Responder::error(718, "ConflictInMappingEntry"),
-        )
-        .await;
+    // server
+    //     .mock(
+    //         Action::add_port_mapping().with_external_port(80),
+    //         Responder::error(718, "ConflictInMappingEntry"),
+    //     )
+    //     .await;
 
-    server.mock(
-        Action::GetStatusInfo,
-        Responder::success()
-            .with_connection_status("Connected")
-            .with_last_connection_error("ERROR_NONE")
-            .with_uptime(86400)  // 1日 = 86400秒
-    ).await;
+    // server.mock(
+    //     Action::GetStatusInfo,
+    //     Responder::success()
+    //         .with_connection_status("Connected")
+    //         .with_last_connection_error("ERROR_NONE")
+    //         .with_uptime(86400)  // 1日 = 86400秒
+    // ).await;
 
     println!("Server is running. Press Ctrl+C to stop.");
 
@@ -51,6 +54,17 @@ async fn main() {
     tokio::signal::ctrl_c()
         .await
         .expect("Failed to listen for Ctrl+C");
+
+    println!();
+    println!("Received SSDP Requests:");
+    for req in server.received_ssdp_requests().await.iter() {
+        println!("-------------------------------------------------------------");
+        println!("source: {:?}", req.source);
+        println!("search_target (ST header value): {:?}", req.search_target);
+        println!("MAN header value: {:?}", req.man);
+        println!("MX header value (maximum wait time in seconds): {:?}", req.mx);
+    }
+    println!("-------------------------------------------------------------");
 
     println!();
     println!("Received Requests:");
